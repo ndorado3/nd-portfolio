124e921ec30f1fe889eb364423e66e1b0ed6a2fe
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-07-02 15:00:25 -0400

    coded the component NavBar

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
+import React from "react";
+import NavBar from "./components/Navbar";
 
+export default function App() {
+    return (
+      <ul>
+        <NavBar />
+      </ul>
+    );
+  }
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+function NavBar({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav flex-column">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a     href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#projects"
+          onClick={() => handlePageChange('Projects')}
+          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>
+          Projects
+        </a>
+      </li>
+      <li className="nav-item">
+        <a href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact-me"
+          onClick={() => handlePageChange("Contact")}
+          className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact Me
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavBar;
