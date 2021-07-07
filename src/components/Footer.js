ad393d1814232b6ba445017674e1f6dcd041a0cc
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-07-07 12:34:58 -0400

    I added the styling to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,51 @@ import React from "react";
 
 import linkInicon from "../Assets/img/linkedIn-icon.png";
 import gitHubicon from "../Assets/img/logo-github.png";
+import "../Assets/css/Footer.css";
 
 export default function Footer() {
   return (
-    <footer className="footer mt-aouto font-small">
-      <div class="conatiner text-end p-5 py-3">
-        <a className="li-ic" href="https://www.linkedin.com/in/nadia-dorado/">
-          <img src={linkInicon} alt="linkIn icon" />
-        </a>
-        <a className="li-ic" href="https://github.com/ndorado3">
-          <img src={gitHubicon} alt="linkIn icon" />
-        </a>
+    <footer class="page-footer font-small unique-color-dark">
+      <div className="container ">
+        {/* <!-- Grid row--> */}
+        <div className="row py-4 d-flex align-items-center">
+          {/* <!-- Grid column --> */}
+          <div className="col-md-6 col-lg-5 text-center text-left mb-4 mb-md-0">
+            <h6 className="mb-0">
+              Designed and Developed by Nadia Dorado
+              <br /> © 2021 Copyright
+            </h6>
+          </div>
+
+          <div className="col-md-6 col-lg-7 text-center text-lg-right">
+            {/* <a className="fb-ic">
+              <i className="fab fa-facebook-f white-text mr-4"> </i>
+            </a> */}
+
+            {/* <!--Linkedin --> */}
+            <a
+              className="li-ic"
+              href="https://www.linkedin.com/in/nadia-dorado/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkInicon} alt="linkIn" width="45" height="45" />
+              <i className="white-text mr-4"> </i>
+            </a>
+            {/* <!--GitHub--> */}
+            <a
+              className="ins-ic"
+              href="https://github.com/ndorado3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={gitHubicon} alt="gitHub" width="35" height="35" />
+              <i className=" white-text"> </i>
+            </a>
+          </div>
+          {/* <!-- Grid column --> */}
+        </div>
+        {/* <!-- Grid row--> */}
       </div>
     </footer>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,13 +15,13 @@ function NavBar({ currentPage, handlePageChange }) {
         Nd
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav" className="style" >
-        <Nav className="mr-auto">
+      <Navbar.Collapse id="responsive-navbar-nav" className="style">
+        <Nav className="mr-auto ">
           <Nav.Link
             href="#about"
             onClick={() => handlePageChange("About")}
             // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-            className={currentPage === "About" ? "nav-link" : "nav-link"}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
           >
             about Me
           </Nav.Link>
@@ -61,75 +61,3 @@ function NavBar({ currentPage, handlePageChange }) {
 }
 
 export default NavBar;
-
-/* <nav className="navbar navbar-expand-lg navbar-dark sticky-top">
-      <div className="container-fluid">
-        <a
-          className="navbar-brand"
-          href="#home"
-          onClick={() => handlePageChange("Home")}
-          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Nd
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <ul className="navbar-nav me-auto">
-            <li class="nav-item">
-              <a
-                href="#about"
-                onClick={() => handlePageChange("About")}
-                // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                className={currentPage === "About" ? "nav-link" : "nav-link"}
-              >
-                about Me
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                href="#projects"
-                onClick={() => handlePageChange("Projects")}
-                className={
-                  currentPage === "Projects" ? "nav-link active" : "nav-link"
-                }
-              >
-                projects
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
-                className={
-                  currentPage === "Resume" ? "nav-link active" : "nav-link"
-                }
-              >
-                resume
-              </a>
-            </li>
-            <li class="nav-item">
-              <a
-                href="#contact-me"
-                onClick={() => handlePageChange("Contact")}
-                className={
-                  currentPage === "Contact" ? "nav-link active" : "nav-link"
-                }
-              >
-                contact Me
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav> */
