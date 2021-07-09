d570de169729c35ba871295f827a30b609ce3e75
nadiadorado@gmail.com <ndorado@uncc.edu>
2021-07-09 00:32:29 -0400

    I fixed the misspelling that I had in the projects page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route,} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./Navbar";
 import Home from "./pages/Home";
@@ -37,36 +37,3 @@ export default function App() {
     </Router>
   );
 }
-
-// export default function PortfolioContainer() {
-//   const [currentPage, setCurrentPage] = useState("Home");
-
-//   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
-//   const renderPage = () => {
-//     if (currentPage === "Home") {
-//       return <Home />;
-//     }
-//     if (currentPage === "About") {
-//       return <About />;
-//     }
-//     if (currentPage === "Projects") {
-//       return <Projects />;
-//     }
-//     if (currentPage === "Resume") {
-//       return <Resume />;
-//     }
-//     return <Contact />;
-//   };
-
-//   const handlePageChange = (page) => setCurrentPage(page);
-
-//   return (
-//     <div>
-//       {/* We are passing the currentPage from state and the function to update it */}
-//       <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-//       {/* Here we are calling the renderPage method which will return a component  */}
-//       {renderPage()}
-//       <Footer />
-//     </div>
-//   );
-// }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,7 +6,7 @@ import "../../Assets/css/Projects.css";
 export default function Projects(props) {
   return (
     <div>
-      <h1 className="title">journy of learning</h1>
+      <h1 className="title">my journey of learning</h1>
       <h4 className="subtitle">check out my projects below <br/>that were developed during my school journey </h4>
       <CardGroup />
     </div>
